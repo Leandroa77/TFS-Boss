860bd42d0573a2e5d37248cc2c4c4902c2e1d7ce
GonzaloVeron <gonveron96@gmail.com>
2020-11-10 04:17:33 -0300

    HitText

--- TweenTest.gd
+++ TweenTest.gd
@@ -2,18 +2,17 @@ extends Label
 
 onready var TweenNode = $Tween
 var value = 1
-
-func _ready():
-	set_process(true)
+var baseSize = 16
+var size = 16
 
 func _process(delta):
 	
 	if Input.is_action_just_pressed("test_izq"):
-		incrementHit(value)
+		toChange(value)
 		value += 1
 	
-	if(value > 4):
-		value = 1
+	if(value > 6):
+		value = 0
 
 func changeColorRed():
 	set("custom_colors/font_color", Color(1,0,0))
@@ -25,19 +24,40 @@ func incrementHit(value):
 	match value:
 		1:
 			text = "1 Hit"
-			get("custom_fonts/font").set("size", 16)
+			changeFontSize(16)
+			changeColorWhite()
+			set("modulate", Color(1, 1, 1, 1))
 			MoveLeft()
 		2:
 			text = "2 Hits"
-			get("custom_fonts/font").set("size", 20)
+			changeFontSize(20)
 		3:
 			#Aca va el sonido
 			text = "3 Hits"
-			changeColorRed()
-			get("custom_fonts/font").set("size", 25)
+			power()
+			changeFontSize(25)
 		_:
 			MoveRight()
+
+func toChange(value):
+	match value:
+		0:
+			MoveRight()
+		1:
+			text = "1 Hit"
+			size = baseSize
 			changeColorWhite()
+			set("modulate", Color(1, 1, 1, 1))
+			changeFontSize(size)
+			MoveLeft()
+			changeColorWhite()
+		_:
+			text = str(value) + " Hit"
+			size += 3
+			get("custom_fonts/font").set("size", min(size, 25))
+			if(value == 3):
+				#Aca va el sonido
+				power()
 
 func MoveLeft():
 	var newPos = Vector2(get_rect().position.x - 500, get_rect().position.y)
@@ -48,3 +68,10 @@ func MoveRight():
 	var newPos = Vector2(get_rect().position.x + 500, get_rect().position.y)
 	TweenNode.interpolate_property(self, "rect_position", get_rect().position, newPos, 1.0, Tween.TRANS_BACK, Tween.EASE_OUT)
 	TweenNode.start()
+
+func power():
+	set("modulate", Color(1.5, 1.5, 1.5, 1))
+	changeColorRed()
+
+func changeFontSize(value):
+	get("custom_fonts/font").set("size", value)
