a6a11af10c054a37fc6cfc3efe1cf27b1064fcc7
Eric Arnez <ericarnez@gmail.com>
2020-11-11 11:59:04 -0300

    algunos fixes.

--- ForDemoPurposes/IconoGodot.gd
+++ ForDemoPurposes/IconoGodot.gd
@@ -9,6 +9,7 @@ func hitted():
 	$Explosion.detonate_explosion()
 	$Sprite.visible = false
 	$CollisionShape2D.disabled = true
+	$Timer.start()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,3 +20,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Timer_timeout():
+	queue_free()
+	pass # Replace with function body.
