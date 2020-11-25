da018fdc434ee07f9703ca752323f547253fb638
GonzaloVeron <gonveron96@gmail.com>
2020-11-25 09:16:03 -0300

    lvl

--- Levels/LevelsForBoss/StageGonza/StageGonza.gd
+++ Levels/LevelsForBoss/StageGonza/StageGonza.gd
@@ -2,7 +2,10 @@ extends Node
 
 onready var enemies = $Enemies
 onready var character = $Character
+onready var soundPlayer = $SoundPlayer
 signal playBackgroundMusic(value)
+
+var actualCheckpoint = Vector2(-7570.123, 4139.863)
 
 func _ready():
 	emit_signal("playBackgroundMusic", 1)
--- SoundPlayer/SoundPlayer.gd
+++ SoundPlayer/SoundPlayer.gd
@@ -2,24 +2,30 @@ extends Node
 
 onready var fxPlayer = $FxPlayer
 onready var bgPlayer = $BgPlayer
+onready var fxEnemy = $FxEnemy
 onready var music = load("res://sounds/BackgroundMusic.ogg")
 onready var explosion = load("res://sounds/Explosion.ogg")
 onready var grappHook = load("res://sounds/GrapplingHook.ogg")
 onready var tackle = load("res://sounds/Tackle.ogg")
 onready var missile_explosion = load("res://sounds/explosion_missile.ogg")
 onready var missile = load("res://sounds/missile.ogg")
+onready var portalG = load("res://sounds/portalGonza.ogg")
 
 var bgSoundsMap = {}
 var fxSoundsMap = {}
+var fxEnemySoundsMap = {}
 
 func _ready():
 	fxSoundsMap[0] = null
 	fxSoundsMap[1] = explosion
 	fxSoundsMap[2] = grappHook
 	fxSoundsMap[3] = tackle
-	fxSoundsMap[4] = missile_explosion
-	fxSoundsMap[5] = missile
+	fxSoundsMap[6] = portalG
 	
+	fxEnemySoundsMap[0] = null
+	fxEnemySoundsMap[1] = missile_explosion
+	fxEnemySoundsMap[2] = missile
+
 	bgSoundsMap[0] = null
 	bgSoundsMap[1] = music
 
@@ -50,3 +56,35 @@ func _on_EnemyFloor_sound_missile(value):
 func _on_StageGonza_playBackgroundMusic(value):
 	bgPlayer.stream = bgSoundsMap[value]
 	bgPlayer.play()
+
+func _on_PortalParticula_playFx(value):
+	fxPlayer.stream = fxSoundsMap[value]
+	fxPlayer.play()
+
+func _on_PortalParticula2_playFx(value):
+	fxPlayer.stream = fxSoundsMap[value]
+	fxPlayer.play()
+
+func _on_PortalParticula3_playFx(value):
+	fxPlayer.stream = fxSoundsMap[value]
+	fxPlayer.play()
+
+func _on_EnemyFloor2_sound_missile(value):
+	fxEnemy.stream = fxEnemySoundsMap[value]
+	fxEnemy.play()
+
+func _on_EnemyFloor3_sound_missile(value):
+	fxEnemy.stream = fxEnemySoundsMap[value]
+	fxEnemy.play()
+
+func _on_EnemyFloor5_sound_missile(value):
+	fxEnemy.stream = fxEnemySoundsMap[value]
+	fxEnemy.play()
+
+func _on_EnemyFloor7_sound_missile(value):
+	fxEnemy.stream = fxEnemySoundsMap[value]
+	fxEnemy.play()
+
+func _on_EnemyFloor9_sound_missile(value):
+	fxEnemy.stream = fxEnemySoundsMap[value]
+	fxEnemy.play()
